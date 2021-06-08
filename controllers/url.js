f5dbc8a99a7741688a5e48202cb6550e42f5c36d
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-08 14:32:06 -0300

    urlEndpoint

--- controllers/url.js
+++ controllers/url.js
@@ -1,9 +1,11 @@
-const { response } = require('express');
-const { Information } = require('../models/information');
-const encryptor = require('simple-encryptor')(process.env.ENCRYPTPASSWORD);
+const { response } = require('express')
+const { Information } = require('../models/information')
+const encryptor = require('simple-encryptor')(process.env.ENCRYPTPASSWORD)
+const {mpLinkGenerator} = require('../helpers/mpLinkGenerator')
+
 
 const generateUrl = async(req, res = response) => {
-    const {bank_transfer=0, crypto_transfer=0, mp_transfer=0, amount, duration='24'} = req
+    const {bank_transfer=0, crypto_transfer=0, mp_transfer=0, amount, duration='24'} = req.body
     
     const reqData = {
         bank_transfer,
@@ -12,8 +14,7 @@ const generateUrl = async(req, res = response) => {
         amount,
         duration
     }
-
-    const url = new Information(reqData)
+    
     const lastInformation = await Information.findAll({
         limit: 1,
         where: {
@@ -21,16 +22,22 @@ const generateUrl = async(req, res = response) => {
         order: [ [ 'createdAt', 'DESC' ]]
     })
     const lastId = lastInformation[0].id
-    //await url.save()
-    const encryptedId = encryptor.encrypt(lastId);
-    const decrypted = encryptor.decrypt(encryptedId);
+    const encryptedId = encryptor.encrypt(lastId)
+
+    const newRegister = new Information({...reqData, url:process.env.hostUrl+encryptedId})
+    await newRegister.save()
+
+    if(mp_transfer===1){            //MercadoPago
+        const mpLink = await mpLinkGenerator(amount)
+        res.json({
+            msg: {
+                newRegister,
+                mercadoPago:mpLink.body.init_point
+            }
+        })
+    }
 
 
-
-    res.json({
-        msg: encryptedId
-    })
-   
 }
 
 
---
+++ helpers/mpLinkGenerator.js
@@ -0,0 +1,31 @@
+const mercadopago = require ('mercadopago');
+
+const mpLinkGenerator = async (amount) =>{
+    
+    mercadopago.configure({
+        access_token: process.env.MERCADOPAGOTOKEN
+    });
+    const preference = {
+        items: [
+            {
+                title:'SUPERSISTEMASWEB',
+                unit_price: 123,
+                quantity:1,
+            }
+        ],
+        back_urls:{
+            success: 'https://www.google.com/search?q=success+page&rlz=1C1ALOY_esAR944AR944&oq=succes&aqs=chrome.0.69i59j69i57j0i20i263i457j0i395i433j46i395i433j0i433j69i60l2.1813j1j4&sourceid=chrome&ie=UTF-8',
+            failure: 'https://www.google.com/search?q=ErrorDocument&rlz=1C1ALOY_esAR944AR944&oq=error&aqs=chrome.0.69i59j69i57j69i59j0i433j0i395i433j69i61l2j69i60.416j1j9&sourceid=chrome&ie=UTF-8',
+            pending: 'https://www.google.com/search?q=pending&rlz=1C1ALOY_esAR944AR944&oq=pending&aqs=chrome..69i57j35i39j35i19i39j0i433j69i60j69i61j69i60l2.536j1j9&sourceid=chrome&ie=UTF-8'
+        },
+        auto_return: 'approved'
+    };
+    
+    const url = await mercadopago.preferences.create(preference)
+    return url
+
+}
+
+module.exports={
+    mpLinkGenerator
+}
