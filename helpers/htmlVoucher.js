9cf85e6e0f98860504e26da25c7fb766ba2ca1e1
Agustin Aguilera <agustin.aguilera424@gmail.com>
2021-06-29 22:40:37 -0300

    voucher 0.5

--- helpers/htmlVoucher.js
+++ helpers/htmlVoucher.js
@@ -1,3 +1,5 @@
+const fs     = require('fs');
+
 const htmlVoucher = ({ date, amount, platform, encryptedId, status }) => {
     return (`
     <!DOCTYPE html>
@@ -7,12 +9,23 @@ const htmlVoucher = ({ date, amount, platform, encryptedId, status }) => {
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Comprobante</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap"
+      rel="stylesheet"
+    />
   </head>
-  <body>
+  <body style="font-family: 'Roboto', sans-serif">
     <div>
       <div
         style="
-          border: 2px solid black;
+          background: rgba(255, 255, 255, 0.25);
+          box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+          backdrop-filter: blur(4px);
+          -webkit-backdrop-filter: blur(4px);
+          border-radius: 10px;
+          border: 1px solid rgba(255, 255, 255, 0.18);
           width: 500px;
           padding: 30px 13px;
           position: relative;
@@ -22,10 +35,12 @@ const htmlVoucher = ({ date, amount, platform, encryptedId, status }) => {
           overflow-wrap: break-word;
         "
       >
-        <h3 style="text-align: center">SuperSistemasWeb</h3>
+          <img style='align:center;margin: auto; width: 130px; height:50px' src="https://www.pagosx.com/logoSS-PX.jpg"/>
         <hr />
         <p>Fecha de pago: ${date}</p>
-        <p>Platform: ${platform===undefined?('Transferencia Bancaria'):(platform)}</p>
+        <p>
+          Platform: ${platform===undefined?('Transferencia Bancaria'):(platform)}
+        </p>
         <p>Monto: $${amount}</p>
         <p>Transacción: ${encryptedId}</p>
         <p>Estado: ${status}</p>
@@ -33,6 +48,8 @@ const htmlVoucher = ({ date, amount, platform, encryptedId, status }) => {
     </div>
   </body>
 </html>
+
+    
 
     `)
 }
